ccd0969a973a7b63bfb2e4d44930eb79f06516d9
alekz29 <konfucjussz29@gmail.com>
2018-05-19 18:47:20 +0200

    ready outline

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,7 +3,28 @@ import '../styles/Summary.scss'
 
 const Summary = () => {
     return (
-        <section className={'Summary'}>
+        <section className={'summary-container'}>
+            <div className={'summary'}>
+                <div className={'padlock'}>
+                </div>
+                <div className={'fit'}>
+                    <div className={'progress-bar'}>
+                    </div>
+                    <div className={'progress-bar'}>
+                    </div>
+                    <p>Aby zobaczyć dane kontaktowe kandydata, zaproś go do dalszego etapu rekrutacji.
+                        Dane zostaną ujawnione, gdy kandydat przyjmie Twoje zaproszenie.</p>
+                </div>
+                <div className={'result'}>
+                    <p>DOPASOWANIE:</p>
+                    <h1>76%</h1>
+                </div>
+            </div>
+            <div className={'action'}>
+                <a>DOPASOWANY W INNYCH PROCESACH(4)...</a>
+                <button>PRZENIEŚ</button>
+                <button>ZAPROŚ</button>
+            </div>
         </section>
     )
 }
