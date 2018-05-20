58b1ad51d8c986cc1b2d6eb3bffe3ec9767354f4
alekz29 <konfucjussz29@gmail.com>
2018-05-20 14:39:14 +0200

    refactoring summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,26 +4,30 @@ import '../styles/Summary.scss'
 const Summary = () => {
     return (
         <section className={'summary-container'}>
+            <div className={'padlock'}>
+            </div>
             <div className={'summary'}>
-                <div className={'padlock'}>
-                </div>
-                <div className={'fit'}>
-                    <div role={'progressbar'} className={'progress-bar'}>
+                <div className={'fit-container'}>
+                    <div className={'fit'}>
+                        <div role={'progressbar '} className={'fit__progress-bar fit__progress-bar--small'}>
+                        </div>
+                        <div role={'progressbar '} className={'fit__progress-bar fit__progress-bar--big'}>
+                        </div>
+                        <p className={'fit__statement'}>Aby zobaczyć dane kontaktowe kandydata, zaproś go do dalszego
+                            etapu
+                            rekrutacji.
+                            Dane zostaną ujawnione, gdy kandydat przyjmie Twoje zaproszenie.</p>
                     </div>
-                    <div role={'progressbar'} className={'progress-bar'}>
+                    <div className={'action'}>
+                        <a className={'action__matching'}>DOPASOWANY W INNYCH PROCESACH (4)...</a>
+                        <button className={'action-button'}>PRZENIEŚ</button>
                     </div>
-                    <p className={'statement'}>Aby zobaczyć dane kontaktowe kandydata, zaproś go do dalszego etapu rekrutacji.
-                        Dane zostaną ujawnione, gdy kandydat przyjmie Twoje zaproszenie.</p>
                 </div>
                 <div className={'result'}>
                     <p className={'result__fit'}>DOPASOWANIE:</p>
                     <h1 className={'result__sum'}>76%</h1>
+                    <button className={'result__button'}>Zaproś</button>
                 </div>
-            </div>
-            <div className={'action'}>
-                <a className={'action__matching'}>DOPASOWANY W INNYCH PROCESACH (4)...</a>
-                <button className={'action__button-move'}>PRZENIEŚ</button>
-                <button className={'action__button-invite'}>Zaproś</button>
             </div>
         </section>
     )
